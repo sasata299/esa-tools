14f16cb2985ee0546fbd3c5052465179833eb859
sasata299 <karaage299@gmail.com>
2019-01-26 09:14:50 +0900

    starを付けたページも開けるようにする

--- esa-opener.go
+++ esa-opener.go
@@ -14,9 +14,22 @@ func main() {
   app.Version = "0.0.1"
 
   app.Action = func (context *cli.Context) error {
-    var url = "https://" + os.Getenv("ESA_TEAM_NAME") + ".esa.io/members/" + context.Args().Get(0)
+    var url string
+    if context.Bool("star") {
+      url = "https://" + os.Getenv("ESA_TEAM_NAME") + ".esa.io/stars"
+    } else {
+      url = "https://" + os.Getenv("ESA_TEAM_NAME") + ".esa.io/members/" + context.Args().Get(0)
+    }
+
     browser.OpenURL(url)
     return nil
+  }
+
+  app.Flags = []cli.Flag{
+    cli.BoolFlag {
+      Name: "star, s",
+      Usage: "starを付けたページを開く",
+    },
   }
 
   app.Run(os.Args)
